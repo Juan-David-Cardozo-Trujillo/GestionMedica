e1157141c198f7b059bdb0213e544bb1e2317f94
juanda010105 <juanda010105@hotmail.com>
2025-12-06 18:12:54 -0500

    Ya estan repositorios

---
+++ src/main/java/com/gestion_medica/demo/Repositorio/EnfermedadRepository.java
@@ -0,0 +1,14 @@
+package com.gestion_medica.demo.Repositorio;
+
+import com.gestion_medica.demo.model.*;
+import com.gestion_medica.demo.model.keys.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public interface EnfermedadRepository extends JpaRepository<Enfermedad, Integer> {
+}
